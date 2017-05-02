0285ac5765be0f6ad54090d63194b532da240863
Simon Bacquie <simon.bacquie@creativedrive.com>
2017-05-02 07:49:18 -0400

    Blanked out some stuff so we can fill it in together

--- app/controllers/api/todos_controller.rb
+++ app/controllers/api/todos_controller.rb
@@ -1,52 +1,21 @@
 class Api::TodosController < ::ApiController
 
   def index
-    # list all todos and render in JSON format
-
-    user_todos = Todo.where(user_id: current_user.id)
-    render status: 200, json: user_todos
   end
 
   def index_done
-    user_todos = Todo.where(user_id: current_user.id, done: true)
-    render status: 200, json: user_todos
   end
 
   def index_open
-    user_todos = Todo.where(user_id: current_user.id, done: false)
-    render status: 200, json: user_todos
   end
 
   def create
-    # make a new Todo object and persist it to the DB
-
-    if params[:name].present?
-      new_todo         = Todo.new
-      new_todo.name    = params[:name]
-      new_todo.user_id = current_user.id
-      new_todo.save
-
-      render status: 201, json: new_todo
-    else
-      render status: 422, json: {message: "'name' form field is required"}
-    end
   end
 
   def update
-    todo = Todo.find(params[:id])
-    todo.name = params[:name] if params[:name].present?
-    todo.done = (params[:name] == 'true') if params[:done].present?
-    todo.save
-
-    # when it's a 204 you don't output any response body
-    render status: 204, json: nil
   end
 
   def destroy
-    todo = Todo.find(params[:id])
-    todo.destroy
-
-    render status: 204, json: nil
   end
 
 end
--- app/jobs/set_all_todos_done_job.rb
+++ app/jobs/set_all_todos_done_job.rb
@@ -3,9 +3,5 @@ class SetAllTodosDoneJobJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    Todo.where(done: false).each do |p|
-      p.visible = true
-      p.save
-    end
   end
 end
